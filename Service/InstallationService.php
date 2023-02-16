8242d741f6faa8ee4ffd8849ed9d29cee6c337fd
Barry Brands <barrybrands02@hotmail.com>
2023-02-16 10:08:44 +0100

    xml endpoint added

--- Service/InstallationService.php
+++ Service/InstallationService.php
@@ -38,6 +38,7 @@ class InstallationService implements InstallerInterface
 
     public const ENDPOINTS = [
         ['path' => 'stuf/zds', 'throws' => ['zds.inbound'], 'name' => 'zds-endpoint', 'methods' => []],
+        ['path' => 'stuf/xml', 'throws' => ['xml.inbound'], 'name' => 'xml-endpoint', 'methods' => []],
     ];
 
     public const SOURCES = [
